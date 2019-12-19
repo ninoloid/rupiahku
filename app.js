47cd4de52e66e6425d433a2ce68f5785c0f01cc5
ninoloid <muhammadsatriaadiputra@gmail.com>
2019-12-20 01:40:27 +0700

    setting heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const User = require('./controllers/userController')
 const session = require('express-session')
 const isLoggedIn = require('./middleware/isLoggedIn')
 const app = express()
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3000
 
 app.use(session({
   secret: 'ninoloid',
